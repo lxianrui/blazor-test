f5314da934f3faf523c5cae89bda817f61e8d803
Jeffrey Liu <jeff16@foxmail.com>
2020-08-19 23:13:09 +0800

    添加项目文件。

---
+++ BlazorApp2/Data/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BlazorApp2.Data
+{
+    public class UserInfo
+    {
+        [DisplayName("编号")]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
---
+++ BlazorApp2/Data/UserInfoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Data
+{
+    public class UserInfoService
+    {
+        List<UserInfo> data = new List<UserInfo>();
+        public UserInfoService()
+        {
+            for (int i = 0; i < 50_000; i++)
+            {
+                data.Add(new UserInfo() { Created = DateTime.Now, Id = Guid.NewGuid(), Name = i.ToString() });
+            }
+        }
+        public Task<UserInfo[]> GetUserList(int index,int size)
+        {
+            return Task.FromResult(Enumerable.Range((index-1)*size,size+1).Select(c=>new UserInfo()
+            {
+                Name = c.ToString(),
+                Id = Guid.NewGuid(),
+                Created = DateTime.Now.AddSeconds(new Random().Next(10000))
+            }).ToArray());
+        }
+    }
+}
